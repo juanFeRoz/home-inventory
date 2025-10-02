ef787dbcbe8167bd2296cd7496499ae390f8cbd2
juferoz <juanfeliperojas2@protonmail.com>
2025-10-02 14:28:19 +0000

    added docker dev enviroment for backend

---
+++ backend/src/test/java/com/example/home_inventory/HomeInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
